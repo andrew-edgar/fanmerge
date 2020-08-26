b6f6e98c436b791e9336a7cb45bf6bd2263c89a9
Andrew Edgar <aedgar@ca.ibm.com>
2020-08-26 11:47:08 -0600

    Add debug to print merged results

--- connect_result.go
+++ connect_result.go
@@ -53,12 +53,13 @@ func mergeResponse(left, right *response) *response {
 		right.response.MsgHdr.Rcode == dns.RcodeSuccess {
 			// We want to randomly pick how to merge this
 			if rand.Intn(1000) % 2 == 1 {
-				log.Info("append right to left")
+				log.Debug("append right to left")
 				left.response.Answer = append(left.response.Answer,right.response.Answer...)
 			} else {
-				log.Info("append left to right")
+				log.Debug("append left to right")
 				left.response.Answer = append(right.response.Answer,left.response.Answer...)
 			}
+			log.Debug("merged response is ", left.response.Answer)
 			return left
 	}
 	return nil
